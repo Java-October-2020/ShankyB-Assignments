71c88a4d40afbc1db2e1924100dc0bbc05fb71a1
Shanky B <shankybakshi@gmail.com>
2020-10-03 09:36:30 -0500

    second commit

---
+++ Week1/FizzBizz/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest{
+        public static void main(String[] args){
+            int i= 4;
+            FizzBuzz iD = new FizzBuzz();
+            String fb= iD.fizzBuzz(i);
+            System.out.println(fb);
+        }       
+}
