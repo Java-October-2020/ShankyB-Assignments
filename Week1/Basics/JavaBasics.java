f7869457cfcca454a4f48ee755a1475d94166abb
Shanky B <shankybakshi@gmail.com>
2020-10-05 01:11:18 -0500

    javabasic master file commit

---
+++ Week1/Basics/JavaBasics.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class JavaBasics {
+
+        public void oneTo255(){
+            for(int i=1; i<256;i++){
+                System.out.println(i);
+            }
+        }
+        
+        public void oddOneTo255(){
+            for(int i=1; i<256;i++){
+                if (i%2!=0){
+                    System.out.println(i);
+                }
+            }
+        }
+
+        public void sumOneTo255(){
+            int sum=0;
+            for(int i=1; i<256;i++){
+            sum= sum+i;     
+            }
+            System.out.println(sum);
+        }
+
+        public void iterateArr(int[] arr){
+            for(int i=0; i<arr.length;i++){
+                System.out.println(arr[i]);
+            }
+
+        }
+
+        public void findMaxArr(int[] arr){
+            int max= arr[0];
+            for(int i=0; i<arr.length;i++){
+                if(max<arr[i]){
+                    max= arr[i]; 
+                }               
+            }   
+            System.out.println(max);
+        }
+
+        public void findAverage(int[] Array){
+            double sum=0;
+        for(int i=0; i<Array.length;i++){
+            sum= sum+Array[i];
+        }
+        int arrayItems= Array.length;
+        double average= sum/arrayItems;
+        System.out.println(sum);
+        System.out.println(average);
+        }
+
+        public void oddElementsArray(int[] Array){
+            ArrayList<Object> OddElementArray= new ArrayList<Object>();
+            for(int i=0; i<Array.length;i++){
+                if(Array[i]%2==1){
+                    OddElementArray.add(Array[i]);
+                }
+            }
+            System.out.println(OddElementArray);
+        }
+
+        public int greaterThanY(int[] Array, int y){
+            ArrayList<Integer> greaterthanY = new ArrayList<Integer>();
+            for (int i=0; i<Array.length;i++){
+                if(myArray[i]>Y){
+                greaterthanY.add(myArray[i]);
+                }
+            }
+            System.out.println(greaterthanY);
+        }
+}
+            
+
+        
+
+
+
+        
+
+    }
--- Week1/Puzzling/02-TaskTwo/Class.java
+++ Week1/Puzzling/02-TaskTwo/Class.java
@@ -2,22 +2,23 @@
 //Shuffle the array and print the name of each person. 
 //Have the method also return an array with names that are longer than 5 characters.
 import java.util.*;
-public class Class{
-        public static void main(String[] args){
-            ArrayList<String> Array = new ArrayList<String>();
-            Array.add("Nancy");
-            Array.add("Jinichi");
-            Array.add("Fujibayashi");
-            Array.add("Momochi");
-            Array.add("Ishikawa"); 
-            Collections.shuffle(Array);
-            System.out.println(Array);
-            System.out.println(Array.get(0));
-            System.out.println(Array.get(1));
-            System.out.println(Array.get(2));
-            System.out.println(Array.get(3));
-            System.out.println(Array.get(4));
-        }
+public class Class{        
+           public  ArrayList<String> Array = new ArrayList<String>();
+                Array.add("Nancy");
+                Array.add("Jinichi");
+                Array.add("Fujibayashi");
+                Array.add("Momochi");
+                Array.add("Ishikawa"); 
+                Collections.shuffle(Array);
+                System.out.println(Array);
+                System.out.println(Array.get(0));
+                System.out.println(Array.get(1));
+                System.out.println(Array.get(2));
+                System.out.println(Array.get(3));
+                System.out.println(Array.get(4));
+                }
+
+            //Have the method also return an array with names that are longer than 5 characters.
 }
 
 
